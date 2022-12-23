4cc280f54b4dacdca9196cac377c34cdce29f18f
Jamie.Nisbet <jamie.nisbet@apoyar.eu>
2022-12-23 14:34:24 +0100

    bid list item improve

--- src/components/ResponsiveTable.jsx
+++ src/components/ResponsiveTable.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { daysTill } from '../utils/dates';
+import { FaGavel } from 'react-icons/fa';
 
 const ResponsiveTable = ({ type, array }) => {
+  const storeState = useSelector((state) => state);
+  const { auth } = storeState;
   return (
     <>
       {type === 'bids' && (
@@ -20,23 +24,37 @@ const ResponsiveTable = ({ type, array }) => {
                   </a>
 
                   <div className='text-gray-600 hover:text-gray-900 relative m-5 font-medium leading-6 transition duration-150 ease-out'>
-                    <span className='block'>{el.title}</span>
+                    <span className='block'>
+                      <b className='underline'>Title</b>
+                      <br />
+                      {el.title}
+                    </span>
                   </div>
                   <div className='text-gray-600 hover:text-gray-900 relative m-5 font-medium leading-6 transition duration-150 ease-out'>
-                    <span className='block'>{el.category}</span>
+                    <span className='block'>
+                      <b className='underline'>Category</b>
+                      <br />
+                      {el.category}
+                    </span>
                   </div>
                   <div className='text-gray-600 hover:text-gray-900 relative m-5 font-medium leading-6 transition duration-150 ease-out'>
-                    <span className='block'>{el.description}</span>
+                    <span className='block'>
+                      <b className='underline'>Budget</b>
+                      <br />
+                      {el.minBudget} - {el.maxBudget}
+                    </span>
                   </div>
 
                   <div className='z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end'>
-                    <a
-                      href='#'
-                      className='text-gray-600 whitespace-no-wrap border-gray-200 hover:bg-gray-50 inline-flex items-center justify-center rounded-md border bg-white px-4 py-2 text-base font-medium leading-6 shadow-sm focus:shadow-none focus:outline-none'
-                      data-rounded='rounded-md'
-                    >
-                      {el.status}
-                    </a>
+                    {auth.user.userRole !== 2 && (
+                      <a
+                        href='#'
+                        className='text-gray-600 whitespace-no-wrap border-gray-200 hover:bg-gray-50 inline-flex items-center justify-center rounded-md border bg-white px-4 py-2 text-base font-medium leading-6 shadow-sm focus:shadow-none focus:outline-none'
+                        data-rounded='rounded-md'
+                      >
+                        {el.participants.length} <FaGavel />
+                      </a>
+                    )}
                     {daysTill(el.endDate) > 0 && (
                       <a
                         href='#'
@@ -46,7 +64,7 @@ const ResponsiveTable = ({ type, array }) => {
                         closes in {daysTill(el.endDate)} days
                       </a>
                     )}
-                    {el.status === 'open' && (
+                    {el.status === 'open' ? (
                       <span className='inline-flex rounded-md shadow-sm'>
                         <a
                           href='#'
@@ -55,6 +73,17 @@ const ResponsiveTable = ({ type, array }) => {
                           data-primary='blue-600'
                         >
                           Bid Now
+                        </a>
+                      </span>
+                    ) : (
+                      <span className='inline-flex rounded-md shadow-sm'>
+                        <a
+                          href='#'
+                          className='whitespace-no-wrap border-blue-700 hover:bg-blue-700 focus:ring-blue-500 inline-flex items-center justify-center rounded-md border bg-error px-4 py-2 text-base font-medium leading-6 text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 lg:mx-1'
+                          data-rounded='rounded-md'
+                          data-primary='blue-600'
+                        >
+                          Closed
                         </a>
                       </span>
                     )}
